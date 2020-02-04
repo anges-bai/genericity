66f60ba7b70c400054a89aa7b967aec4c6cab718
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-02-04 18:54:32 +0800

    Create foreach.java

---
+++ foreach.java
@@ -0,0 +1,10 @@
+public class TestDemo {
+public static void main(String[] args) {
+int[] data = new int[] { 1, 2, 3, 4, 5 }; 
+// 原始数组
+for (int i = 0; i < data.length; i++) {
+System.out.println(data[i]);
+// 通过循环控制索引下标 
+}
+}
+}
